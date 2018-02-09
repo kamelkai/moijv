6b82ae592638f1c742eb89fc5e960aa8841c204b
kamel <kamelkailech@hotmail.com>
2018-02-09 17:40:30 +0100

    Add-constraints

---
+++ src/Validator/Constraints/BadWordValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class BadWordValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        /* @var $constraint BadWord */
+        foreach ($constraint->badwords as $badword){
+            if(strpos($value, $badword) !== FALSE){
+                $this->context->buildViolation($constraint->message)
+                        ->setParameter('{{ value }}', $value)
+                        ->setParameter('{{ badword }}', $badword)
+                        ->addViolation();
+            }
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+}
